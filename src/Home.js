f7fc0eabcb1648458ea991e9108dca7b42b85a43
Barun Debnath <barundebnath91@gmail.com>
2022-01-15 21:23:13 +0530

    Rendering list

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,20 @@
 import React, { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("mario");
-
-  const [age, setAge] = useState(25);
-
-  const handleClick = () => {
-    setName("luigi");
-    setAge(30);
-  };
+  const [blogs, setBlogs] = useState([
+    { title: "ABC", body: "lorem ipsum", author: "mario", id: 1 },
+    { title: "DEF", body: "lorem ipsum", author: "yoshi", id: 2 },
+    { title: "GHI", body: "lorem ipsum", author: "luigi", id: 3 },
+  ]);
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age}
-      </p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
