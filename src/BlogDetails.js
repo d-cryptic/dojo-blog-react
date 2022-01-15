aa2461d4d036e8ae47e9a573ba734c93a31717e7
Barun Debnath <barundebnath91@gmail.com>
2022-01-15 23:28:27 +0530

    route parameters

---
+++ src/BlogDetails.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const BlogDetails = () => {
+  const { id } = useParams();
+
+  return (
+    <div className="blog-details">
+      <h2>Blog Details - {id}</h2>
+    </div>
+  );
+};
+
+export default BlogDetails;
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
