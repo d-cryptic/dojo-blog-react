f5001e87a94aa679cf3043a7543c3bdd3aada4bc
Barun Debnath <barundebnath91@gmail.com>
2022-01-15 22:37:57 +0530

    Conditional Loading Message

--- src/Home.js
+++ src/Home.js
@@ -2,26 +2,26 @@ import React, { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "ABC", body: "lorem ipsum", author: "mario", id: 1 },
-    { title: "DEF", body: "lorem ipsum", author: "yoshi", id: 2 },
-    { title: "GHI", body: "lorem ipsum", author: "luigi", id: 3 },
-  ]);
-
-  const [name, setName] = useState(null);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-      });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {isPending && <div>Loading...</div>}
+
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
