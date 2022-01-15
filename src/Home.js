b94d1880d39c48c1f466a985f8c891b66332a064
Barun Debnath <barundebnath91@gmail.com>
2022-01-15 22:09:37 +0530

    state lifting

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -7,14 +8,14 @@ const Home = () => {
     { title: "GHI", body: "lorem ipsum", author: "luigi", id: 3 },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
     </div>
   );
 };
