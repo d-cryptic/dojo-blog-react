51825093a5a281bf8cbaa9cc2556bb996c642b3d
Barun Debnath <barundebnath91@gmail.com>
2022-01-15 18:22:21 +0530

    styling added

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>The Dojo Blog</h1>
+      <div className="link">
+        <a href="/"></a>
+        <a href="/create">New Blog</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
