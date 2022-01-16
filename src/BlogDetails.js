9903bcff5847cdc412d0b73cca7587ea81876f98
Barun Debnath <barundebnath91@gmail.com>
2022-01-16 21:50:19 +0530

    delete blog added

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
@@ -10,6 +10,16 @@ const BlogDetails = () => {
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
 
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
+
   return (
     <div className="blog-details">
       {isPending && <div>Loading...</div>}
@@ -19,6 +29,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
