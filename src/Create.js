7ae82202bbaf4b55884e0c70b64ccb4e1be04e62
Barun Debnath <barundebnath91@gmail.com>
2022-01-16 21:45:15 +0530

    Pragmattic redirects

--- src/Create.js
+++ src/Create.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
-
   const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,6 +20,8 @@ const Create = () => {
       body: JSON.stringify(blog),
     }).then(() => {
       setIsPending(false);
+      //   history.go(-1);
+      history.push("/");
     });
   };
 
